6ee5125c110acb28effff96887fc24a9cbecc872
Preston Sego <LPSego3+dev@gmail.com>
2018-04-19 09:41:27 -0400

    typescript

---
+++ types/emberclear/index.d.ts
@@ -0,0 +1,10 @@
+
+import Ember from 'ember';
+
+declare global {
+  interface Array<T> extends Ember.ArrayPrototypeExtensions<T> {}
+  // interface Function extends Ember.FunctionPrototypeExtensions {}
+}
+
+export {};
+
