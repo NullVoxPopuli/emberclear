25e78e0feead48ce6c135b6e245660dac17ed5a3
Preston Sego <LPSego3+dev@gmail.com>
2018-04-27 11:04:56 -0400

    sigh

--- src/services/message-dispatcher.js
+++
@@ -1,8 +0,0 @@
-import Service from '@ember/service';
-import { inject as service } from '@ember/service';
-
-export default Service.extend({
-  toast: service('toast'),
-  redux: service('redux'),
-  relayConnection: service('relay-connection'),
-})
---
+++ src/services/message-dispatcher.ts
@@ -0,0 +1,8 @@
+import Service from '@ember/service';
+import { inject as service } from '@ember/service';
+
+export default class MessageDispatcher extends Service {
+  toast = service('toast');
+  redux = service('redux');
+  relayConnection = service('relay-connection');
+}
