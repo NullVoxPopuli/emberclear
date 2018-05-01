b3fa5d6b983e4ef86d4fe18725ae9b7b651901c6
Preston Sego <LPSego3+dev@gmail.com>
2018-05-01 14:09:39 -0400

    could be worse

--- src/resolver.js
+++ src/resolver.js
@@ -1,6 +1,9 @@
 import Resolver from 'ember-resolver/resolvers/fallback';
 // import Resolver from 'ember-resolver/resolvers/glimmer-wrapper';
 
+import { merge } from '@ember/polyfills';
+
+
 import buildResolverConfig from 'ember-resolver/ember-config';
 import config from '../config/environment';
 
@@ -9,6 +12,15 @@ let moduleConfig = buildResolverConfig(config.modulePrefix);
  * If your application has custom types and collections, modify moduleConfig here
  * to add support for them.
  */
+
+ merge(moduleConfig.types, {
+  config: { definitiveCollection: 'main' },
+  locale: { definitiveCollection: 'main'},
+  'ember-i18n@config': { definitiveCollection: 'main' },
+  'util': { definitiveCollection: 'utils' }
+});
+
+moduleConfig.collections.main.types.push('config');
 
 export default Resolver.extend({
   config: moduleConfig
