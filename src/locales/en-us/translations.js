5e5626067fd682046d86aa7469817057fa5508bc
Preston Sego <LPSego3+dev@gmail.com>
2018-05-08 15:51:25 -0400

    some i18n-ing

--- src/locales/en-us/translations.js
+++ src/locales/en-us/translations.js
@@ -1,5 +1,6 @@
 export default {
   appname: 'emberclear',
+  subheader: 'Encrypted Chat. No History. No Logs.',
 
   connection: {
     connecting: 'Connecting...',
@@ -27,6 +28,39 @@ export default {
       subscribe: {
         notConnected: 'Cannot subscribe to a channel without a socket connection!'
       }
+    }
+  },
+  buttons: {
+    next: 'Next',
+    back: 'Back',
+    begin: 'Begin',
+  },
+  ui: {
+    faq: {
+      title: 'F.A.Q',
+      whatIsQ: 'What is {{appname}}',
+      whatIsA: `
+        EmberClear is the open source p2p encrypted chat
+        client that operates over open source
+        mesh nodes on free-tier cloud services.
+      `,
+      howDoesWorkQ: 'How does it work?',
+      howDoesWorkA: ``,
+      whyQ: 'Why?',
+      whyA: ``,
+    },
+    login: {
+      title: 'Login',
+      instructions: 'Please paste or type your mnemonic key.'
+    },
+    setup: {
+      introQuestion: 'What would you like to be called?',
+      almostReady: 'You are almost ready to begin chatting!',
+      nameLabel: 'This is how others will identify you',
+      mnemonicPrompt: `
+        If you would like to use this account on other computers,
+        please store this mnemonic in a secure place. It will be used
+        to login.`
     }
   }
 }
