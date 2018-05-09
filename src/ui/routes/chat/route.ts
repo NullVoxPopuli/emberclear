0da4d12399f18551ffc6cfcb5bb993c07a1e8a59
Preston Sego <LPSego3+dev@gmail.com>
2018-05-09 13:05:32 -0400

    fix some types

--- src/ui/routes/chat/route.ts
+++ src/ui/routes/chat/route.ts
@@ -3,13 +3,15 @@ import Route from '@ember/routing/route';
 import { service } from '@ember-decorators/service';
 
 import { action } from '@ember-decorators/object';
+import RelayConnection from 'emberclear/services/relay-connection';
+import IdentityService from 'emberclear/services/identity/service';
 
 
 export default class ChatRoute extends Route {
-  @service('relay-connection') relaySocket;
-  @service('identity') identity;
+  @service('relay-connection') relaySocket!: RelayConnection;
+  @service('identity') identity!: IdentityService;
 
-  activate(this: ApplicationRoute) {
+  activate(this: ChatRoute) {
     this.relaySocket.connect();
   }
 
