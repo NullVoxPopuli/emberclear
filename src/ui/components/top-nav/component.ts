7bc10e1f571d057c144b7c872c4fca1aa3e192f7
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-05-21 12:59:39 -0400

    menup works

--- src/ui/components/top-nav/component.ts
+++ src/ui/components/top-nav/component.ts
@@ -1,5 +1,6 @@
 import Component from '@ember/component';
 import { service } from '@ember-decorators/service';
+import { action, computed } from '@ember-decorators/object';
 import { alias, notEmpty } from '@ember-decorators/object/computed';
 
 import IdentityService from 'emberclear/services/identity/service';
@@ -9,4 +10,20 @@ export default class TopNav extends Component {
 
   @alias('identity.record.name') name?: string;
   @notEmpty('identity.record.name') hasName!: boolean;
+
+  isTouchMenuVisible = false;
+
+  @computed('isTouchMenuVisible')
+  get touchMenuClasses() {
+    if (this.isTouchMenuVisible) {
+      return 'is-active';
+    }
+
+    return '';
+  }
+
+  @action
+  toggleTouchMenu() {
+    this.set('isTouchMenuVisible', !this.isTouchMenuVisible);
+  }
 }
