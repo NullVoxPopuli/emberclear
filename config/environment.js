121b0e3cf13148736ae8ad27bf03b4cd64a844a9
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-05-22 21:05:12 -0400

    back to non-ember-cli-deploy

--- config/environment.js
+++ config/environment.js
@@ -30,7 +30,7 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
-    },
+    }
   };
 
   if (environment === 'development') {
