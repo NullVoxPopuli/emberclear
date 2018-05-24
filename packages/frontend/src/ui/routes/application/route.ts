10775084890a186017fcd9df5303053e352e73a1
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-05-24 09:53:23 -0400

    updated icons

--- packages/frontend/src/ui/routes/application/route.ts
+++ packages/frontend/src/ui/routes/application/route.ts
@@ -11,5 +11,13 @@ export default class ApplicationRoute extends Route {
   activate(this: ApplicationRoute) {
     this.identity.load();
     this.relayConnection.connect();
+
+    window.addEventListener("beforeinstallprompt", function(e) {
+      // log the platforms provided as options in an install prompt
+      console.log(e.platforms); // e.g., ["web", "android", "windows"]
+      e.userChoice.then(function(outcome) {
+        console.log(outcome); // either "accepted" or "dismissed"
+      }, handleError);
+    });
   }
 }
