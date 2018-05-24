460fda5f9cf2d11e9434b0b3cc2dd597766c5b49
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-05-24 10:14:48 -0400

    try a loader?

--- packages/frontend/src/ui/routes/application/route.ts
+++ packages/frontend/src/ui/routes/application/route.ts
@@ -7,17 +7,25 @@ import IdentityService from 'emberclear/services/identity/service';
 export default class ApplicationRoute extends Route {
   @service identity!: IdentityService;
   @service relayConnection!: RelayConnection;
+  @service fastboot;
 
   activate(this: ApplicationRoute) {
+    if (this.fastboot.isFastBoot) return;
+
+    const el = document.getElementById('pre-load-spinner');
+    el.parentNode.removeChild(el);
+
     this.identity.load();
     this.relayConnection.connect();
 
-    window.addEventListener("beforeinstallprompt", function(e) {
-      // log the platforms provided as options in an install prompt
-      console.log(e.platforms); // e.g., ["web", "android", "windows"]
-      e.userChoice.then(function(outcome) {
-        console.log(outcome); // either "accepted" or "dismissed"
-      }, handleError);
-    });
+
+
+    // window.addEventListener("beforeinstallprompt", function(e) {
+    //   // log the platforms provided as options in an install prompt
+    //   console.log(e.platforms); // e.g., ["web", "android", "windows"]
+    //   e.userChoice.then(function(outcome) {
+    //     console.log(outcome); // either "accepted" or "dismissed"
+    //   }, handleError);
+    // });
   }
 }
