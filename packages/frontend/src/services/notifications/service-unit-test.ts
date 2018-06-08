0449471e13c73050e72fa7eae5304908c1c53f30
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-06-08 05:43:43 -0400

    comment out code quality and dep scanning

--- packages/frontend/src/services/notifications/service-unit-test.ts
+++ packages/frontend/src/services/notifications/service-unit-test.ts
@@ -6,7 +6,7 @@ module('Unit | Service | notifications', function(hooks) {
 
   // Replace this with your real tests.
   test('it exists', function(assert) {
-    // let service = this.owner.lookup('service:notifications');
-    // assert.ok(service);
+    let service = this.owner.lookup('service:notifications');
+    assert.ok(service);
   });
 });
