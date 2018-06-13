211c945c79b5b68b391b46d8deaec2a78ccd11fb
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-06-12 22:16:59 -0400

    bah

--- packages/frontend/src/ui/components/invite/invite-modal/component.ts
+++ packages/frontend/src/ui/components/invite/invite-modal/component.ts
@@ -1,15 +1,26 @@
+import Ember from 'ember';
 import Component from '@ember/component';
 
 import { service } from '@ember-decorators/service';
 import { computed } from '@ember-decorators/object';
-import { readOnly } from '@ember-decorators/object/computed';
+import { reads } from '@ember-decorators/object/computed';
 import { task } from 'ember-concurrency';
 
 import Identity from 'emberclear/services/identity/service';
 import { convertObjectToQRCodeDataURL, toHex } from 'emberclear/src/utils/string-encoding';
 
 
-export default class InviteModal extends Component {
+export default class InviteModal extends Component.extend({
+  // qrCodeTask: task(function* () {
+  //   let publicIdentity = this.get('publicIdentity');
+  //   let qrCode = yield convertObjectToQRCodeDataURL(publicIdentity);
+  //   return qrCode;
+  // })
+  // .drop()
+  // .observes('publicIdentity'),
+  //
+  // qrCode: Ember.computed.reads('qrCodeTask.last.value')
+}) {
   @service identity!: Identity;
 
   @computed('identity.publicKey', 'identity.name')
@@ -25,19 +36,15 @@ export default class InviteModal extends Component {
     return JSON.stringify(this.publicIdentity, null, 2);
   }
 
+  @reads('qrCodeTask.last.value') qrCode!: string;
+
   qrCodeTask = task(function * (this: InviteModal) {
-    console.log('start qr');
-    const qr = yield convertObjectToQRCodeDataURL(this.publicIdentity);
+    console.log("hey look at me");
+    const publicIdentity = this.publicIdentity;
+    const qrCode = yield convertObjectToQRCodeDataURL(publicIdentity);
 
-    console.log('qr', qr);
-    return qr;
-  }).keepLatest().observes('publicIdentity');
+    return qrCode;
+  }).drop().observes('publicIdentity');
 
-
-  qrCodeTaskInstance(this: InviteModal) {
-    return this.qrCodeTask.perform();
-  }
-
-  @readOnly('qrCodeTaskInstance.value') qrCode!: string;
 
 }
