51c4194587cadd8bdfc6d871dec967f03d541d39
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-06-17 13:58:33 -0400

    try to github pages again

--- packages/frontend/src/ui/components/top-nav/user-drop-menu/component.ts
+++ packages/frontend/src/ui/components/top-nav/user-drop-menu/component.ts
@@ -3,9 +3,11 @@ import { service } from '@ember-decorators/service';
 import { alias } from '@ember-decorators/object/computed';
 import { action } from '@ember-decorators/object';
 import IdentityService from 'emberclear/services/identity/service';
+import { Registry } from '@ember/service';
 
 export default class UserDropMenu extends Component {
   @service identity!: IdentityService;
+  @service router!: Registry['router'];
 
   // tagName = '';
   showDropdown = false;
