eb586177ee45e55a841c3f6e74ed526d1d76fad4
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-06-19 07:53:54 -0400

    update manifest rootURl

--- packages/frontend/config/manifest.js
+++ packages/frontend/config/manifest.js
@@ -4,24 +4,26 @@
 module.exports = function(environment /*, appConfig */) {
   // See https://github.com/san650/ember-web-app#documentation for a list of
   // supported properties
-  const rootURL = environment.rootURL || process.env.ROOT_URL;
+
+  // rootURL should end in a slash
+  const rootURL = environment.rootURL || process.env.ROOT_URL || '/';
 
   return {
     name: 'emberclear',
     short_name: 'emberclear',
     description: 'Encrypted Chat. No History. No Logs.',
-    start_url: `${rootURL || '/'}`,
+    start_url: `${rootURL}`,
     display: 'standalone',
     background_color: '#fff',
     theme_color: '#fff',
     icons: [
       {
-        src: `${rootURL}/assets/images/icons/android-chrome-192x192.png`,
+        src: `${rootURL}assets/images/icons/android-chrome-192x192.png`,
         sizes: '192x192',
         type: 'image/png',
       },
       {
-        src: `${rootURL}/assets/images/icons/android-chrome-512x512.png`,
+        src: `${rootURL}assets/images/icons/android-chrome-512x512.png`,
         sizes: '512x512',
         type: 'image/png',
       },
