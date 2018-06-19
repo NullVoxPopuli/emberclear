4ecea44cf74a43dfb0d8c69cd53991b0cfc61671
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-06-19 18:43:06 -0400

    fix dropdown alignment on rigth

--- packages/frontend/src/utils/promise.ts
+++
@@ -1,8 +0,0 @@
-import EmberObject from '@ember/object';
-import PromiseProxyMixin from '@ember/object/promise-proxy-mixin';
-
-import DS from 'ember-data';
-
-// export const PromiseProxy = DS.PromiseObject;
-
-export const PromiseProxy = EmberObject.extend(PromiseProxyMixin);
