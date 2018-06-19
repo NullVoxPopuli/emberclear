177a3a07da0f2974714c2c92e3e25801e53e362a
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-06-19 05:47:18 -0400

    oops

--- packages/frontend/src/init/initializers/service-worker-prompt.ts
+++ packages/frontend/src/init/initializers/service-worker-prompt.ts
@@ -3,7 +3,7 @@ import Application from '@ember/application';
 // https://love2dev.com/blog/beforeinstallprompt/
 export function initialize(application: Application): void {
   // application.inject('route', 'foo', 'service:foo');
-  if(!window) return;
+  if(!window || typeof(window.addEventListener) !== 'function') return;
 
   window.addEventListener('beforeinstallprompt', event => {
       // Prevent Chrome 67 and earlier from automatically showing the prompt
