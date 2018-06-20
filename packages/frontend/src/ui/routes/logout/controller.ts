432c2568c82497591f39c59ee2da05e7d9467436
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-06-19 22:31:10 -0400

    Fix logout

--- packages/frontend/src/ui/routes/logout/controller.ts
+++ packages/frontend/src/ui/routes/logout/controller.ts
@@ -1,12 +1,18 @@
 import Controller from '@ember/controller';
 import localforage from 'localforage';
 import { action } from '@ember-decorators/object';
+import { service } from '@ember-decorators/service';
+import IdentityService from 'emberclear/services/identity/service';
 
 
 export default class LogoutController extends Controller {
+  @service identity!: IdentityService;
+
   @action
   logout(this: LogoutController) {
     this.store.unloadAll('identity');
+    this.identity.set('record', undefined);
+
     localforage.clear();
 
     this.transitionToRoute('application');
