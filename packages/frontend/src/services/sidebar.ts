05a0192a79527cc446f4b08f1451c584800adb34
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-07-17 05:16:22 -0400

    fix fastboot rendering

--- packages/frontend/src/services/sidebar.ts
+++ packages/frontend/src/services/sidebar.ts
@@ -1,15 +1,23 @@
 import Service from '@ember/service';
 import { computed } from '@ember-decorators/object';
+import { service } from '@ember-decorators/service';
 
 export default class Sidebar extends Service {
+  @service fastboot!: FastBoot;
+
   @computed
   get isShown() {
+    if (this.fastboot.isFastBoot) return false;
+
     const lsValue = localStorage.getItem('sidebarIsShown');
 
     return  lsValue === 'true';
   }
   set isShown(value: boolean) {
+    if (this.fastboot.isFastBoot) return;
+
     const lsValue = value ? 'true' : 'false';
+
     localStorage.setItem('sidebarIsShown', lsValue);
   }
 
