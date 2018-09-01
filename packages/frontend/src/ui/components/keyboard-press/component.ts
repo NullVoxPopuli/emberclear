1ac644649cbeaec237211b7dbed627fcf8d67e04
NullVoxPopuli <lpsego3+dev@gmail.com>
2018-09-01 22:56:33 +0000

    Keyboard Shortcuts and Accessibility

--- packages/frontend/src/ui/components/keyboard-press/component.ts
+++ packages/frontend/src/ui/components/keyboard-press/component.ts
@@ -1,17 +1,42 @@
 import Component from '@ember/component';
-import { EKMixin, keyDown } from 'ember-keyboard';
+import { EKMixin, keyDown, keyPress, keyUp, EKOnInsertMixin } from 'ember-keyboard';
 
-const KeyboardAwareComponent = Component.extend(EKMixin);
+const KeyboardAwareComponent = Component.extend(EKMixin, EKOnInsertMixin, {
+  keyboardFirstResponder: true
+});
 
 export default class KeyboardPress extends KeyboardAwareComponent {
-  // key = null;
-  // onPress = () => {};
-
   didInsertElement() {
     this._super(...arguments);
 
-    this.set('keyboardActivated', true);
+    const {
+      key,
+      onDown, onPress, onUp
+    } = this;
 
-    this.on(keyDown(this.key), this.onPress);
+
+    if (onDown) {
+      this.on(keyDown(key), this.eventHandler(onDown));
+    }
+
+    if (onPress) {
+      this.on(keyPress(key), this.eventHandler(this.onPress));
+    }
+
+    if (onUp) {
+      this.on(keyUp(key), this.eventHandler(onUp));
+    }
   }
+
+
+  eventHandler(fn: () => void) {
+    return (event: KeyboardEvent) => {
+      event.preventDefault();
+      event.stopPropagation();
+
+      fn();
+    }
+  }
+
+
 }
