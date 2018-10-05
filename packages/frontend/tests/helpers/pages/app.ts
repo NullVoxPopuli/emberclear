eac60b715e581524aa05d6cac28c9d68c53fb890
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-10-05 18:02:32 -0400

    some more tests around logout

--- packages/frontend/tests/helpers/pages/app.ts
+++ packages/frontend/tests/helpers/pages/app.ts
@@ -5,6 +5,12 @@ const toast = '[data-test-notification-message]';
 export const app = {
   toast: () => find(toast),
   dismissToast: () => click(toast),
+
+  userDropdown: {
+    open: () => click('[data-test-user-dropdown-toggle]'),
+    clickLogout: () => click('[data-test-user-dropdown] [data-test-logout]'),
+    logoutButton: () => find('[data-test-user-dropdown] [data-test-logout]')
+  }
 }
 
 export default {
