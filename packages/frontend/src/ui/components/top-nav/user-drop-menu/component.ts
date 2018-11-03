4e935c3514d12af40255c232feb14f697b315210
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-11-02 22:16:11 -0400

    style updates, fix dropdown

--- packages/frontend/src/ui/components/top-nav/user-drop-menu/component.ts
+++ packages/frontend/src/ui/components/top-nav/user-drop-menu/component.ts
@@ -1,25 +1,44 @@
 import { DS } from 'ember-data';
-import Component from '@ember/component';
+import Component, { tracked } from 'sparkles-component';
+import uuid from 'uuid';
+import { later } from '@ember/runloop';
 
 import { service } from '@ember-decorators/service';
-import { alias } from '@ember-decorators/object/computed';
-import { action } from '@ember-decorators/object';
+import { reads } from '@ember-decorators/object/computed';
 
 import { Registry } from '@ember/service';
 import IdentityService from 'emberclear/services/identity/service';
+import { keepInViewPort } from 'emberclear/src/utils/dom/utils';
 
 export default class UserDropMenu extends Component {
   @service identity!: IdentityService;
   @service store!: DS.Store;
   @service router!: Registry['router'];
 
-  showDropdown = false;
-  classNameBindings = ['showDropdown:is-active']
+  @tracked showDropdown = false;
+  @tracked dropDownId = uuid();
 
-  @alias('identity.name') name?: string;
+  @reads('identity.name') name?: string;
 
-  @action
-  closeMenu(this: UserDropMenu) {
-    this.set('showDropdown', false);
+  get dropDown(): HTMLElement {
+    return document.querySelector(`[id="${this.dropDownId}"]`) as HTMLElement;
   }
+
+  closeMenu() {
+    this.showDropdown = false;
+  }
+
+  openMenu() {
+    this.showDropdown = true;
+
+    later(this, () => keepInViewPort(this.dropDown));
+  }
+
+  toggleMenu() {
+    this.showDropdown
+    ? this.closeMenu()
+    : this.openMenu();
+  }
+
+
 }
--- packages/frontend/src/utils/dom/utils.ts
+++ packages/frontend/src/utils/dom/utils.ts
@@ -1,6 +1,6 @@
 export function isElementWithin(element: HTMLElement, container: HTMLElement): boolean {
-  var rect = element.getBoundingClientRect();
-  var containerRect = container.getBoundingClientRect();
+  const rect = element.getBoundingClientRect();
+  const containerRect = container.getBoundingClientRect();
 
   const isVisible = (
     rect.top >= containerRect.top &&
@@ -12,3 +12,24 @@ export function isElementWithin(element: HTMLElement, container: HTMLElement): b
   return isVisible;
 }
 
+export function keepInViewPort(element: HTMLElement, margin = 20 /* px */) {
+  const rect = element.getBoundingClientRect();
+
+  if (rect.left < 0) {
+    element.style.left = `${margin}px`;
+  }
+
+  if (rect.right > window.innerWidth) {
+    const delta = window.innerWidth - rect.right;
+
+    element.style.left = `${delta - margin}px`;
+  }
+
+  if (rect.top < 0) {
+    element.style.top = `${margin}px`;
+  }
+
+  if (rect.bottom > window.innerHeight) {
+    element.style.bottom = `${margin}px`;
+  }
+}
