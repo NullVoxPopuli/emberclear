ee9cf0ab8643ad93485c2a46ce603a23ab4e6f84
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-12-06 06:11:46 -0500

    add add friend button to contacts page

--- packages/frontend/src/ui/routes/contacts/-components/header/component.ts
+++ packages/frontend/src/ui/routes/contacts/-components/header/component.ts
@@ -1,15 +1,21 @@
-import Component from '@ember/component';
+import Component from 'sparkles-component';
 import { filter } from '@ember-decorators/object/computed';
 import { service } from '@ember-decorators/service';
 
 import Identity from 'emberclear/data/models/identity/model';
 import IdentityService from 'emberclear/services/identity/service';
+import ModalService from 'emberclear/services/modals';
 
 export default class extends Component {
   @service identity!: IdentityService;
+  @service modals!: ModalService;
 
-  @filter('identities')
+  @filter('args.identities')
   contacts(identity: Identity) {
     return identity.id !== this.identity.id;
+  }
+
+  addFriend() {
+    this.modals.toggle('add-friend');
   }
 }
