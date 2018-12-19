8d0194ab59a10b804f2ce2d66aef393c308acd26
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-12-19 05:57:32 -0500

    When there is an Update Available notification, clicking the banner should refresh the page, not take you to the home screen.

---
+++ packages/frontend/src/ui/routes/application/-components/update-checker/component.ts
@@ -0,0 +1,9 @@
+import Component from 'sparkles-component';
+import { service } from '@ember-decorators/service';
+import { reads } from '@ember-decorators/object/computed';
+
+export default class UpdateChecker extends Component {
+  @service router!: Router;
+
+  @reads('router.currentURL') currentURL!: string;
+}
