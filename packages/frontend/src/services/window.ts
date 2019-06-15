d399e63dc369d37efde27b096f1eb31ce03fbf29
NullVoxPopuli <LPSego3+dev@gmail.com>
2019-06-15 09:25:53 -0400

    Update window.ts

--- packages/frontend/src/services/window.ts
+++ packages/frontend/src/services/window.ts
@@ -1,5 +1,4 @@
 import Service from '@ember/service';
-import { tracked } from '@glimmer/tracking';
 import { monitor } from 'ember-computed-promise-monitor';
 import { computed } from '@ember/object';
 
