861172e7eef5f8649025bdc2817d6510532582fa
NullVoxPopuli <LPSego3+dev@gmail.com>
2019-09-08 19:00:38 -0400

    blegh

--- scripts/github/deploy.js
+++ scripts/github/deploy.js
@@ -11,63 +11,34 @@ const {
 } = process.env;
 
 
-const cwd = process.cwd();
-
-const STATUS = {
-  ERROR: 'error',
-  FAILURE: 'failure',
-  INACTIVE: 'inactive',
-  IN_PROGRESS: 'in_progress',
-  QUEUED: 'queued',
-  PENDING: 'pending',
-  SUCCESS: 'success',
-}
-
-let deployId = undefined;
-let url = '';
+const [_node, _script, status, url] = process.argv;
 
 const tasks = new Listr([
   {
     title: 'Setting Deployment Status: In Progress',
-    task: () => updateStatus(deployId, STATUS.IN_PROGRESS),
+    task: () => updateStatus(STATUS.IN_PROGRESS),
   },
-  {
-    title: 'Install Dependencies',
-    task: () => execa.sync('./run yarn install', { cwd }),
-  },
-  {
-    title: 'Build for Production',
-    task: () => execa.sync('./run yarn build:production', { cwd }),
-  },
-  {
-    title: 'Deploying to Netlify',
-    task: () => url = deploy(),
-  },
-  {
-    title: 'Updating Deployment Status: Success',
-    task: () => updateStatus(deployId, STATUS.SUCCESS, url),
-  }
 ]);
 
-function deploy( ){
-  let dir = 'packages/frontend/dist';
-  let { stdout } = execa.sync(`netlify deploy --dir=${dir} --json`);
+// function deploy( ){
+//   let dir = 'packages/frontend/dist';
+//   let { stdout } = execa.sync(`netlify deploy --dir=${dir} --json`);
 
-  console.log(stdout, typeof stdout);
-  let response = JSON.parse(stdout);
+//   console.log(stdout, typeof stdout);
+//   let response = JSON.parse(stdout);
 
-  let deployUrl = response['deploy_url'];
+//   let deployUrl = response['deploy_url'];
 
-  console.log(`
-    Deploy Preview URL
-    ${deployUrl}
-  `);
+//   console.log(`
+//     Deploy Preview URL
+//     ${deployUrl}
+//   `);
 
-  return deployUrl;
-}
+//   return deployUrl;
+// }
 
 
-function updateStatus(deployId, status, targetUrl = '') {
+function updateStatus() {
   return fetch(`${GITHUB_API_URL}/repos/${GITHUB_REPOSITORY}/statuses/${GITHUB_SHA}`, {
     method: 'POST',
     headers: {
@@ -78,17 +49,14 @@ function updateStatus(deployId, status, targetUrl = '') {
     },
     data: {
       state: status,
-      ['target_url']: targetUrl || `https://github.com/${GITHUB_REPOSITORY}`,
+      ['target_url']: url || `https://github.com/${GITHUB_REPOSITORY}`,
       description: 'Deploy Preview'
     }
   });
 }
 
 
-tasks.run().catch(async err => {
-  try {
-    await updateStatus(deployId, STATUS.FAILURE);
-  } finally {
-    process.exit(1);
-  }
+tasks.run().catch(err => {
+  console.error(err);
+  process.exit(1);
 });
