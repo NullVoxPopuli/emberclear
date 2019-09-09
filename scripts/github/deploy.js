2896e48b892d7bd91631843465463ef70f8771e9
NullVoxPopuli <LPSego3+dev@gmail.com>
2019-09-08 20:38:56 -0400

    what does this do?

--- scripts/github/deploy.js
+++ scripts/github/deploy.js
@@ -3,60 +3,80 @@
 const execa = require('execa');
 const Listr = require('listr');
 const fetch = require('node-fetch');
+const Octokit = require('@octokit/rest')
 
 const GITHUB_API_URL="https://api.github.com"
 
 const {
-  GITHUB_REPOSITORY, GITHUB_TOKEN, GITHUB_SHA
+  GITHUB_REPOSITORY, GITHUB_TOKEN, GITHUB_SHA,
+  GITHUB_REF
 } = process.env;
 
 
-const [_node, _script, status, url] = process.argv;
-
-const tasks = new Listr([
-  {
-    title: 'Setting Deployment Status: In Progress',
-    task: () => updateStatus(),
-  },
-]);
-
-// function deploy( ){
-//   let dir = 'packages/frontend/dist';
-//   let { stdout } = execa.sync(`netlify deploy --dir=${dir} --json`);
-
-//   console.log(stdout, typeof stdout);
-//   let response = JSON.parse(stdout);
-
-//   let deployUrl = response['deploy_url'];
-
-//   console.log(`
-//     Deploy Preview URL
-//     ${deployUrl}
-//   `);
-
-//   return deployUrl;
-// }
+const BEGIN='begin';
+const SUCCESS='success';
+const FAILURE='failure';
+const [_node, _script, stage, url] = process.argv;
 
 
-function updateStatus() {
-  return fetch(`${GITHUB_API_URL}/repos/${GITHUB_REPOSITORY}/statuses/${GITHUB_SHA}`, {
-    method: 'POST',
-    headers: {
-      ['Authorization']: `token ${GITHUB_TOKEN}`,
-      // ['Content-Type']: `text/json; charset=utf-8`,
-      // ['Accept']: '',
-      // ['User-Agent']: 'NullVoxPopuli'
-    },
-    data: {
-      state: status,
-      ['target_url']: url || `https://github.com/${GITHUB_REPOSITORY}`,
-      description: 'Deploy Preview'
-    }
-  });
+const octokit = new Octokit({
+  auth: `token ${ GITHUB_TOKEN }`,
+});
+
+const [owner, repo] = GITHUB_REPOSITORY.split('/');
+
+
+async function begin() {
+    let result = await octokit.checks.create({
+      owner,
+      repo,
+      name: 'Deploy Preview',
+      head_sha: GITHUB_SHA
+    });
+
+  console.log(result);
 }
 
+async function success( ){
+    let result = await octokit.checks.update({
+      owner,
+      repo,
+      status: 'completed',
+      conclusion: 'success',
+      details_url: url,
+      check_run_id
+    });
 
-tasks.run().catch(err => {
-  console.error(err);
-  process.exit(1);
-});
+  console.log(result);
+}
+
+function failure() {
+    let result = octokit.checks.update({
+      owner,
+      repo,
+      status: 'completed',
+      conclusion: 'failure',
+      check_run_id
+    });
+
+  console.log(result);
+}
+
+switch (stage) {
+  case BEGIN:
+    return new Listr([
+      {
+        title: 'Setting Up Status Check',
+        task: () => {
+          return begin();
+        }
+      }
+    ]).run().catch(err => {
+        console.error(err);
+    });
+  case SUCCESS:
+    return success();
+  case FAILURE:
+    return failure();
+}
+
