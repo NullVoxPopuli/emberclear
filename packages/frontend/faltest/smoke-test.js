b01ea19258c092376aca4c85ca1040b5cbe3148e
Kelly Selden <kellyselden@gmail.com>
2019-09-28 19:36:11 +0100

    more faltest progress (#166)
    * more faltest progress
    
    * use new getAttribute
    
    * temporary regression fix

--- packages/frontend/faltest/smoke-test.js
+++ packages/frontend/faltest/smoke-test.js
@@ -35,9 +35,35 @@ describe('smoke', function() {
     await this.server;
   });
 
-  it('works', async function() {
+  it('works #browser1 #browser2', async function() {
     let title = await this.browser.getTitle();
 
     assert.strictEqual(title, 'emberclear');
+
+    await this.browser.click('[href="/chat"]');
+
+    let alias = Math.random()
+      .toString(36)
+      .substr(2);
+
+    await this.browser.setValue('[placeholder="Your desired alias"]', alias);
+
+    await this.browser.click('[value="Next"]');
+
+    await this.browser.click('[href="/chat"]');
+
+    // element click intercepted: Element <a class="button button-xs" href="/add-friend">...</a> is not clickable at point (274, 76). Other element would receive the click: <a class="service-worker-update-notify alert alert-info has-shadow" href="/chat" style="z-index: 100;">...</a>
+    // await this.browser.click('[href="/add-friend"]');
+    await this.browser.execute(() => {
+      // eslint-disable-next-line no-undef
+      document.querySelector('[href="/add-friend"]').click();
+    });
+
+    let friendLink = await this.browser.getAttribute(
+      '[data-clipboard-text]',
+      'data-clipboard-text'
+    );
+
+    assert.ok(friendLink.length > 0);
   });
 });
