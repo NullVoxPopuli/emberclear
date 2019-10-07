53af3fa7615cb483bad48ee97a7c919190c6404e
NullVoxPopuli <LPSego3+dev@gmail.com>
2019-10-07 10:51:22 -0400

    extract startServer helper and add linting files

---
+++ packages/smoke-tests/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  plugins: ['node', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'prettier',
+  ],
+  env: {
+    mocha: true,
+    node: true,
+    es6: true,
+  },
+  rules: {
+    ...require('eslint-plugin-node').configs.recommended.rules,
+    'node/no-unpublished-require': 'off', // we live dangerously here
+    'node/no-extraneous-require': 'off', // incorrect?
+
+    // cleanliness & consistency
+    'prefer-const': 'off', // const has misleading safety implications
+    'no-console': ['error', { allow: ['debug', 'warn', 'error', 'info'] }],
+    'no-cond-assign': 'off',
+    'no-useless-escape': 'off',
+    'require-yield': 'off',
+    'getter-return': 'off',
+
+    // prettier
+    'prettier/prettier': 'error',
+
+  },
+};
---
+++ packages/smoke-tests/.prettierrc.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'es5',
+  printWidth: 80,
+  semi: true,
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  tabs: false,
+  tabWidth: 2,
+};
