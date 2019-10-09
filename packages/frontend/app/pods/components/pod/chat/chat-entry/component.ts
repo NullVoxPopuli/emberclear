f8e982a98ecb51d6e4232f98396f10d3b1c4a4e3
Austin Gassert <amgassert@gmail.com>
2019-10-08 21:53:01 -0400

    Fix emoji insertions in the middle of a message

--- packages/frontend/app/pods/components/pod/chat/chat-entry/component.ts
+++ packages/frontend/app/pods/components/pod/chat/chat-entry/component.ts
@@ -79,9 +79,10 @@ export default class ChatEntry extends Component<IArgs> {
   }
 
   @action onInput(event: KeyboardEvent) {
+    const EMOJI_REGEX = /:[^:]+:/g;
     const value = (event.target as any).value;
 
-    if (value.charAt(value.length - 1) === ':') {
+    if (value.match(EMOJI_REGEX)) {
       this.text = unicode(value);
     } else {
       this.text = value;
