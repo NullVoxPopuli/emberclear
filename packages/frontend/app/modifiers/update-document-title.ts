2bb9fc814278b35ce6bcca83e5242587d3a3af0f
CelticMajora <cdonoho16@gmail.com>
2019-10-15 20:44:47 -0400

    Ran lint --fix

--- packages/frontend/app/modifiers/update-document-title.ts
+++ packages/frontend/app/modifiers/update-document-title.ts
@@ -1,25 +1,26 @@
-import Modifier from "ember-class-based-modifier";
+import Modifier from 'ember-class-based-modifier';
 
 export default class UpdateDocumentTitle extends Modifier {
-    originalDocumentTitle: string;
+  originalDocumentTitle: string;
 
-    constructor(owner, args){
-        super(owner, args);
-        this.originalDocumentTitle = document.title;
-    }
+  constructor(owner, args) {
+    super(owner, args);
+    this.originalDocumentTitle = document.title;
+  }
 
-    get unreadMessageCount(){
-        return this.args.named.unreadMessageCount
-    }
-    get currentUserName(){
-        return this.args.named.currentUserName
-    }
+  get unreadMessageCount() {
+    return this.args.named.unreadMessageCount;
+  }
+  
+  get currentUserName() {
+    return this.args.named.currentUserName;
+  }
 
-    willDestroy() {
-        document.title = this.originalDocumentTitle;
-    }
+  willDestroy() {
+    document.title = this.originalDocumentTitle;
+  }
 
-    didReceiveArguments(){
-        document.title = "emberclear " + this.unreadMessageCount + " " + this.currentUserName;
-    }
+  didReceiveArguments() {
+    document.title = 'emberclear ' + this.unreadMessageCount + ' ' + this.currentUserName;
+  }
 }
