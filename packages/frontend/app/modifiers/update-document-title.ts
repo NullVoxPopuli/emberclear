f8a71e924d1dd88c706a527c21ceccca986e700b
CelticMajora <cdonoho16@gmail.com>
2019-10-15 20:37:57 -0400

    Made a very strict modifier. Wish there was something like sprintf in typescript so I could pass a string with tokens and any number of more args to fill in

---
+++ packages/frontend/app/modifiers/update-document-title.ts
@@ -0,0 +1,25 @@
+import Modifier from "ember-class-based-modifier";
+
+export default class UpdateDocumentTitle extends Modifier {
+    originalDocumentTitle: string;
+
+    constructor(owner, args){
+        super(owner, args);
+        this.originalDocumentTitle = document.title;
+    }
+
+    get unreadMessageCount(){
+        return this.args.named.unreadMessageCount
+    }
+    get currentUserName(){
+        return this.args.named.currentUserName
+    }
+
+    willDestroy() {
+        document.title = this.originalDocumentTitle;
+    }
+
+    didReceiveArguments(){
+        document.title = "emberclear " + this.unreadMessageCount + " " + this.currentUserName;
+    }
+}
