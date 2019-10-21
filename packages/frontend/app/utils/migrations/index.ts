509c8314f0e533a984e01ff849d5ce40d1a1841c
L. Preston Sego III <LPSego3+dev@gmail.com>
2019-10-20 21:53:09 -0400

    Remove migrations -- it's been long enough! (#272)
    * Remove data migrations. It's bee long enough
    
    * revert relay submodule

--- packages/frontend/app/utils/migrations/index.ts
+++
@@ -1,7 +0,0 @@
-import ApplicationInstance from '@ember/application/instance';
-
-import { up as v1Tov2 } from 'emberclear/utils/migrations/v1-to-v2';
-
-export async function runMigrations(appInstance: ApplicationInstance) {
-  await v1Tov2(appInstance);
-}
