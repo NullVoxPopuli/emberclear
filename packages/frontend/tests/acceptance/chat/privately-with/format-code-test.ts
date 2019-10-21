425541a4c3347a3ab6df287aaed285186c806eff
NullVoxPopuli <LPSego3+dev@gmail.com>
2019-10-21 10:54:28 -0400

    work on tests

---
+++ packages/frontend/tests/acceptance/chat/privately-with/format-code-test.ts
@@ -0,0 +1,96 @@
+import { module, test, skip } from 'qunit';
+import StoreService from 'ember-data/store';
+import { visit, currentURL, settled, waitFor, triggerEvent } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+import { percySnapshot } from 'ember-percy';
+
+import { stripIndent } from 'common-tags';
+
+import {
+  clearLocalStorage,
+  setupRelayConnectionMocks,
+  setupCurrentUser,
+  getService,
+  visit as visitIgnoringFailure,
+} from 'emberclear/tests/helpers';
+
+import { page, selectors } from 'emberclear/tests/helpers/pages/chat';
+import { createContact } from 'emberclear/tests/helpers/factories/contact-factory';
+import Contact from 'emberclear/models/contact';
+import { waitUntil } from '@ember/test-helpers';
+import { toast } from 'emberclear/tests/helpers/pages/toast';
+
+let codeA = stripIndent`
+  \`\`\`ts
+  let forA = 2;
+  \`\`\`
+`;
+
+let codeB = stripIndent`
+  \`\`\`ts
+  let forB = 3;
+  \`\`\`
+`;
+
+async function submitCodeTo(code: string, to: Contact, assert: Assert) {
+  await visit(`/chat/privately-with/${to.id}`);
+  await page.textarea.fillIn(code);
+  await page.submitButton.click();
+
+  let { hasCode } = page.messages.objectAt(0);
+
+  assert.ok(hasCode, `code for ${to.name} is present`);
+}
+
+module('Acceptance | Chat | Privately With | format-code', function(hooks) {
+  setupApplicationTest(hooks);
+  clearLocalStorage(hooks);
+
+  module('is logged in with two contacts', function(hooks) {
+    setupCurrentUser(hooks);
+    setupRelayConnectionMocks(hooks, {
+      send() {
+        // should something be asserted here?
+      },
+    });
+
+    let contactA!: Contact;
+    let contactB!: Contact;
+
+    hooks.beforeEach(async function() {
+      contactA = await createContact('A Contact A');
+      contactB = await createContact('B Contact B');
+    });
+
+    module('a message with code is sent to Contact A & B', function(hooks) {
+      hooks.beforeEach(async function(assert) {
+        await submitCodeTo(codeA, contactA, assert);
+        await submitCodeTo(codeB, contactB, assert);
+      });
+
+      module('when navigating back to A', function(hooks) {
+        hooks.beforeEach(async function() {
+          await visit(`/chat/privately-with/${contactA.id}`);
+        });
+
+        test('the chat history still renders the code snippet', function(assert) {
+          let { hasCode } = page.messages.objectAt(0);
+
+          assert.ok(hasCode, 'code for A is present');
+        });
+
+        module('when navigating back to B', function(hooks) {
+          hooks.beforeEach(async function() {
+            await visit(`/chat/privately-with/${contactB.id}`);
+          });
+
+          test('the chat history still renders the code snippet', function(assert) {
+            let { hasCode } = page.messages.objectAt(0);
+
+            assert.ok(hasCode, 'code for B is present');
+          });
+        });
+      });
+    });
+  });
+});
