59880b07aee6752b17fa440c86cfb5d208a00e5e
NullVoxPopuli <LPSego3+dev@gmail.com>
2019-10-23 14:41:26 -0400

    Extract codeclimate + test running to a script

---
+++ packages/frontend/scripts/test-with-coverage.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export GIT_COMMIT_SHA=$GITHUB_SHA
+export GIT_BRANCH=${GITHUB_REF#refs/head/}
+
+./cc-test-reporter before-build
+
+time yarn test
+
+./cc-test-reporter format-coverage \
+  --debug \
+  --add-prefix packages/frontend/ \
+  --output ./coverage/coverage.json \
+  --input-type lcov coverage/lcov.info
+
+./cc-test-reporter upload-coverage \
+  --debug \
+  --input ./coverage/coverage.json
