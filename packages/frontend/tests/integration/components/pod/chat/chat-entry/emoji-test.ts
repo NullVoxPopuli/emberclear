d085eeff8f8edbb514e6d3bc63ec8c831cbb4b68
Austin Gassert <amgassert@gmail.com>
2019-10-26 01:29:32 -0400

    Switch to using querySelector to get textarea and other modifications

--- packages/frontend/tests/integration/components/pod/chat/chat-entry/emoji-test.ts
+++ packages/frontend/tests/integration/components/pod/chat/chat-entry/emoji-test.ts
@@ -1,7 +1,8 @@
 import { module, test } from 'qunit';
-import { render, typeIn } from '@ember/test-helpers';
+import { render, fillIn } from '@ember/test-helpers';
 import { setupRenderingTest } from 'ember-qunit';
 import { hbs } from 'ember-cli-htmlbars';
+import { getStore } from 'emberclear/tests/helpers';
 
 module('Integration | Component | chat-entry', function(hooks) {
   setupRenderingTest(hooks);
@@ -9,60 +10,77 @@ module('Integration | Component | chat-entry', function(hooks) {
   module('emoji code replacement', function() {
     module('there are no emoji codes to replace', function() {
       test('result does not contain emoji', async function(assert) {
-        assert.expect(2);
-        await render(hbs`<Pod::Chat::ChatEntry />`);
-        const chatEntry = this.element as HTMLElement;
+        assert.expect(1);
 
-        assert.equal(
-          chatEntry.textContent,
-          '',
-          'The chat entry should initially be empty'
-        );
+        const store = getStore();
+        const testContact = store.createRecord('contact', {name: 'test'});
+        this.set('testContact', testContact);
+        
+        await render(hbs`<Pod::Chat::ChatEntry @to={{this.testContact}} />`);
 
         const testString = 'This is a test string with no emoji codes to replace.';
+        await fillIn('textarea', testString);
 
-        typeIn(chatEntry, testString);
-
+        const textArea = this.element.querySelector('textarea')!;
         assert.equal(
-          chatEntry.textContent,
+          textArea.value,
           testString,
-          'A string with no emoji codes should not be modified'
+          'string with no emoji codes should not be modified'
         );
       });
+
+      // test('emoji codes are not replaced when not between colons', async function(assert) {
+      //   assert.expect(1);
+        
+      //   const store = getStore();
+      //   const testContact = store.createRecord('contact', {name: 'test'});
+      //   this.set('testContact', testContact);
+        
+      //   await render(hbs`<Pod::Chat::ChatEntry @to={{this.testContact}} />`);
+
+      //   const testString = 'scream smile heartheart heart wave';
+      //   fillIn('textarea', testString);
+
+      //   const textArea = this.element.querySelector('textarea')!;
+      //   assert.equal(
+      //     textArea.value,
+      //     testString,
+      //     'emoji codes that are not between colons should not be replaced'
+      //   );
+      // });
     });
 
     module('there are emoji codes to replace', function() {
       test('result contains emoji', async function(assert) {
-        assert.expect(2);
-        await render(hbs`<Pod::Chat::ChatEntry />`);
-        const chatEntry = this.element as HTMLElement;
-
-        assert.equal(
-          chatEntry.textContent,
-          '',
-          'The chat entry should initially be empty'
-        );
+        assert.expect(1);
+        
+        const store = getStore();
+        let testContact = store.createRecord('contact', {name: 'test'});
+        this.set('testContact', testContact);
+        
+        await render(hbs`<Pod::Chat::ChatEntry @to={{this.testContact}} />`);
 
         const testString = `
           This is a test string with emoji codes to replace. :smile:
-          The quick :b:rown fox jumped over the lazy dog.
-          A :dog: and a :cat:.
+          The quick :b:rown fox jumps over the lazy dog.
+          A :dog: and a :cat: napped.
           ¡:b::a::m:!
         `;
 
-        typeIn(chatEntry, testString);
+        fillIn('textarea', testString);
 
+        const textArea = this.element.querySelector('textarea')!;
         const expectedString = `
           This is a test string with emoji codes to replace. 😄
-          The quick 🅱️rown fox jumped over the lazy dog.
-          A 🐶 and a 🐱.
+          The quick 🅱️rown fox jumps over the lazy dog.
+          A 🐶 and a 🐱 napped.
           ¡🅱️🅰️Ⓜ️!
         `;
 
         assert.equal(
-          chatEntry.textContent,
+          textArea.value,
           expectedString,
-          'A string with emoji codes should be modified correctly'
+          'string with emoji codes should be modified correctly'
         );
       });
     });
