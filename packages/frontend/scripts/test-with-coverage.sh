e19b5b8bf3cf98d76bdc981d0082d05161095b17
NullVoxPopuli <LPSego3+dev@gmail.com>
2019-10-27 09:29:42 -0400

    Modify the lcov file myself before sending to test coverage services

--- packages/frontend/scripts/test-with-coverage.sh
+++ packages/frontend/scripts/test-with-coverage.sh
@@ -2,19 +2,26 @@
 
 export GIT_COMMIT_SHA=$GITHUB_SHA
 export GIT_BRANCH=${GITHUB_REF#refs/heads/}
+export COVERAGE=true
 
 ./cc-test-reporter before-build
 
 time yarn test
 
-./cc-test-reporter format-coverage \
-  --debug \
-  --add-prefix packages/frontend/ \
-  --output ./coverage/coverage.json \
-  --input-type lcov coverage/lcov.info
+# fix the paths, since we aren't generating coverage from
+# the root of the mono repo
+sed -i -E "s/^SF:(.+)$/SF:packages\/frontend\/\1/" coverage/lcov.info
 
-./cc-test-reporter upload-coverage \
-  --debug \
-  --input ./coverage/coverage.json
+./cc-test-reporter after-build -t lcov
+
+# --add-prefix packages/frontend/ \
+# ./cc-test-reporter format-coverage \
+#   --debug \
+#   --output ./coverage/coverage.json \
+#   --input-type lcov coverage/lcov.info
+
+# ./cc-test-reporter upload-coverage \
+#   --debug \
+#   --input ./coverage/coverage.json
 
 yarn codecov
