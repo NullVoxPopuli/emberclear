b3d06c3e5a4b0d15f7d601b3376300ba8522a3cb
NullVoxPopuli <LPSego3+dev@gmail.com>
2019-11-01 11:28:06 -0400

    does this increase coverage?

--- packages/frontend/app/pods/components/collapsible/component.ts
+++ packages/frontend/app/pods/components/collapsible/component.ts
@@ -1,11 +1,23 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import { setComponentTemplate } from '@ember/component';
+import { hbs } from 'ember-cli-htmlbars';
 
-export default class Collapsible extends Component {
+class Collapsible extends Component {
   @tracked isOpen = true;
 
   @action toggle() {
     this.isOpen = !this.isOpen;
   }
 }
+
+export default setComponentTemplate(
+  hbs`
+  {{yield this.isOpen
+    this.toggle
+    (component 'collapsible/icon')
+  }}
+`,
+  Collapsible
+);
--- packages/frontend/app/pods/components/keyboard-shortcuts/key/component.ts
+++ packages/frontend/app/pods/components/keyboard-shortcuts/key/component.ts
@@ -1,13 +1,22 @@
 import Component from '@glimmer/component';
+import { setComponentTemplate } from '@ember/component';
+import { hbs } from 'ember-cli-htmlbars';
 
 interface IArgs {
   label: string;
 }
 
-export default class Key extends Component<IArgs> {
+class Key extends Component<IArgs> {
   get i18nKey() {
     const { label } = this.args;
 
     return `ui.shortcuts.key.${label}`;
   }
 }
+
+export default setComponentTemplate(
+  hbs`
+  <i class='key'>{{t this.i18nKey}}</i>
+  `,
+  Key
+);
