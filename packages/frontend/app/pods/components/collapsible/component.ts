75dd9038f06d90b3e3f42ee58c1f7d15eb706ed2
NullVoxPopuli <LPSego3+dev@gmail.com>
2019-11-01 08:14:58 -0400

    remove toggle helper

--- packages/frontend/app/pods/components/collapsible/component.ts
+++ packages/frontend/app/pods/components/collapsible/component.ts
@@ -1,6 +1,11 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class Collapsible extends Component {
   @tracked isOpen = true;
+
+  @action toggle() {
+    this.isOpen = !this.isOpen;
+  }
 }
