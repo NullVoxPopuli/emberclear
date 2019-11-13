575b2ee5fab8a6b308e59f48247eaccb1e57180e
NullVoxPopuli <LPSego3+dev@gmail.com>
2019-11-12 20:09:16 -0500

    moar tests

---
+++ packages/frontend/app/pods/components/pod/settings/interface/-page.ts
@@ -0,0 +1,19 @@
+import { create } from 'ember-cli-page-object';
+
+import { inputDefinition } from 'emberclear/pods/components/switch/-page';
+
+export const definition = {
+  scope: '[data-test-interface]',
+  hideOfflineContacts: {
+    scope: '[data-test-hide-offline-contacts]',
+    ...inputDefinition,
+  },
+  themes: {
+    selectMidnight: {
+      scope: '[data-test-theme-midnight]',
+      ...inputDefinition,
+    },
+  },
+};
+
+export const page = create(definition);
