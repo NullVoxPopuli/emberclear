d3f1a1d24c72c671c03f6c3cbcf3e2a7e9165585
NullVoxPopuli <LPSego3+dev@gmail.com>
2019-12-18 22:16:25 -0500

    renovate is failing me :(
    fix link
    
    eslint changes

--- packages/frontend/.eslintrc.js
+++ packages/frontend/.eslintrc.js
@@ -1,71 +1,86 @@
+let commonRules = {
+  // ember specific
+  'ember/no-new-mixins': 'off', // don't know what to do about ember-keyboard
+  'ember/avoid-leaking-state-in-ember-objects': 'warn',
+  'ember/no-incorrect-calls-with-inline-anonymous-functions': 'off', // don't agree with this one.
+  'ember/no-ember-testing-in-module-scope': 'off', // needed for tasks atm
+  'ember/use-brace-expansion': 'off', // won't matter with @tracked
+
+  // cleanliness & consistency
+  'prefer-const': 'off', // const has misleading safety implications
+  'no-console': [
+    'error',
+    { allow: ['debug', 'warn', 'error', 'info', 'group', 'groupEnd', 'groupCollapsed'] },
+  ],
+  'no-cond-assign': 'off',
+  'no-useless-escape': 'off',
+  'require-yield': 'off',
+  '@typescript-eslint/camelcase': 'off', // temp disable, because route params are snake case
+  'getter-return': 'off',
+
+  // over-enforcement of immutability patterns
+  'require-atomic-updates': 'warn',
+
+  // tests / qunit
+  'qunit/no-identical-names': 'warn', // doesn't support deep nesting
+
+  // prettier
+  'prettier/prettier': 'error',
+};
+
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: ['ember', 'prettier', 'qunit', '@typescript-eslint', 'decorator-position'],
+  parser: 'babel-eslint',
+  plugins: ['ember', 'prettier', 'qunit', 'decorator-position'],
   extends: [
     'eslint:recommended',
     'plugin:ember/recommended',
     'plugin:qunit/recommended',
     'plugin:decorator-position/ember',
-    'plugin:@typescript-eslint/recommended',
     'prettier',
-    'prettier/@typescript-eslint',
   ],
   rules: {
-    // ember specific
-    'ember/no-new-mixins': 'off', // don't know what to do about ember-keyboard
-    'ember/avoid-leaking-state-in-ember-objects': 'warn',
-    'ember/no-incorrect-calls-with-inline-anonymous-functions': 'off', // don't agree with this one.
-    'ember/no-ember-testing-in-module-scope': 'off', // needed for tasks atm
-    'ember/use-brace-expansion': 'off', // won't matter with @tracked
-
-    // cleanliness & consistency
-    'prefer-const': 'off', // const has misleading safety implications
-    'no-console': [
-      'error',
-      { allow: ['debug', 'warn', 'error', 'info', 'group', 'groupEnd', 'groupCollapsed'] },
-    ],
-    'no-cond-assign': 'off',
-    'no-useless-escape': 'off',
-    'require-yield': 'off',
-    '@typescript-eslint/camelcase': 'off', // temp disable, because route params are snake case
-    'getter-return': 'off',
-
-    // over-enforcement of immutability patterns
-    'require-atomic-updates': 'warn',
-
-    // tests / qunit
-    'qunit/no-identical-names': 'warn', // doesn't support deep nesting
-
-    // typescript
-    '@typescript-eslint/prefer-optional-chain': 'warn', // for now
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    '@typescript-eslint/no-use-before-define': 'off',
-    '@typescript-eslint/no-empty-interface': 'off',
-    '@typescript-eslint/explicit-member-accessibility': 'off',
-    // this one has to be disabled, because not all of my deps use real types
-    // ... or, my custom type defs are incorrect
-    '@typescript-eslint/interface-name-prefix': 'off', // ['error', 'always'],
-    // typescript isn't smart enough to know when we _know_ data will exist
-    '@typescript-eslint/no-non-null-assertion': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off', // implicit return types are fine
-
-    // prettier
-    'prettier/prettier': 'error',
-
-    // better handled by prettier:
-    '@typescript-eslint/indent': 'off',
+    ...commonRules,
   },
   overrides: [
-    // {
-    //   files: ['**/*.ts'],
-    //   rules: {
-    //     // Better enforced by TS
-    //     'no-undef': 'off',
-    //     'no-unused-vars': 'off',
-    //     'ember/no-attrs-snapshot': 'off'
-    //   }
-    // },
+    {
+      files: ['**/*.ts'],
+      parserOptions: {
+        project: 'tsconfig.json',
+      },
+      plugins: ['ember', 'prettier', 'qunit', '@typescript-eslint'],
+      extends: [
+        'eslint:recommended',
+        'plugin:ember/recommended',
+        'plugin:qunit/recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'prettier',
+        'prettier/@typescript-eslint',
+        'plugin:decorator-position/ember',
+      ],
+      rules: {
+        // typescript
+        '@typescript-eslint/prefer-optional-chain': 'error',
+        '@typescript-eslint/no-unnecessary-condition': 'error',
+        '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+        '@typescript-eslint/no-use-before-define': 'off',
+        '@typescript-eslint/no-empty-interface': 'off',
+        '@typescript-eslint/explicit-member-accessibility': 'off',
+        // this one has to be disabled, because not all of my deps use real types
+        // ... or, my custom type defs are incorrect
+        '@typescript-eslint/interface-name-prefix': 'off', // ['error', 'always'],
+        // typescript isn't smart enough to know when we _know_ data will exist
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off', // implicit return types are fine
+
+        // better handled by prettier:
+        '@typescript-eslint/indent': 'off',
+
+        ...commonRules,
+      },
+    },
     {
       files: ['app/services/prism-manager.ts'],
       rules: {
@@ -82,9 +97,14 @@ module.exports = {
     //tests
     {
       files: ['**/*-test.ts', 'tests/**'],
+      env: {
+        embertest: true,
+      },
       rules: {
         'ember/no-test-import-export': 'off', // this rule is incomplete
         '@typescript-eslint/no-empty-function': 'off',
+        // doesn't support deep nesting
+        'qunit/no-identical-names': 'off',
       },
     },
     // node files
@@ -117,12 +137,6 @@ module.exports = {
         'node/no-unpublished-require': 'off', // we live dangerously here
         'node/no-extraneous-require': 'off', // incorrect?
       }),
-    },
-    {
-      files: ['faltest/**/*-test.js'],
-      env: {
-        mocha: true,
-      },
     },
   ],
 };
--- packages/frontend/tests/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  env: {
-    embertest: true,
-  },
-  rules: {
-    // doesn't support deep nesting
-    'qunit/no-identical-names': 'warn',
-  },
-};
