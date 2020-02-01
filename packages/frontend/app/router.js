fd90403954fb98bf021715a2e3d29d027dc94051
NullVoxPopuli <LPSego3+dev@gmail.com>
2020-02-01 12:26:38 -0500

    upgrade ember-router-scroll

--- packages/frontend/app/router.js
+++ packages/frontend/app/router.js
@@ -1,11 +1,10 @@
-import EmberRouter from '@ember/routing/router';
-import RouterScroll from 'ember-router-scroll';
+import EmberRouterScroll from 'ember-router-scroll';
 import config from 'emberclear/config/environment';
 
-const Router = EmberRouter.extend(RouterScroll, {
-  location: config.locationType,
-  rootURL: config.rootURL,
-});
+class Router extends EmberRouterScroll {
+  location = config.locationType;
+  rootURL = config.rootURl;
+}
 
 Router.map(function() {
   this.route('chat', function() {
