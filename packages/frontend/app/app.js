f77c03fb8be8dcc4a7029b3125195be0050c1d29
NullVoxPopuli <LPSego3+dev@gmail.com>
2020-02-02 11:13:20 -0500

    Add Sentry to see what information it collects, and to see if I can publicly expose it all

--- packages/frontend/app/app.js
+++ packages/frontend/app/app.js
@@ -3,6 +3,9 @@ import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
+import * as Sentry from '@sentry/browser';
+import * as Integrations from '@sentry/integrations';
+
 import defineModifier from 'ember-concurrency-test-waiter/define-modifier';
 
 export default class App extends Application {
@@ -10,6 +13,11 @@ export default class App extends Application {
   podModulePrefix = config.podModulePrefix;
   Resolver = Resolver;
 }
+
+Sentry.init({
+  dsn: 'https://cf1cbf0969314ecfb5fee114482fccbe@sentry.io/1422271',
+  integrations: [new Integrations.Ember()],
+});
 
 /*
  * This line is added to support initializers in the `app/` directory
