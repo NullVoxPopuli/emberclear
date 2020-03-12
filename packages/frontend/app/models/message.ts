f0aecab08e23a883d5faedff64783874bdfc0f03
CelticMajora <cdonoho16@gmail.com>
2020-03-12 12:26:44 -0400

    ran lint:js --fix

--- packages/frontend/app/models/message.ts
+++ packages/frontend/app/models/message.ts
@@ -22,10 +22,10 @@ export const MESSAGE_LIMIT = 75;
  *
  * DISCONNECT: a courtesy message to notify your contacts that you
  *             are about to go offline.
- * 
- * INFO_CHANNEL_SYNC: a system message that is sent without a body to request channel 
+ *
+ * INFO_CHANNEL_SYNC: a system message that is sent without a body to request channel
  *                    context and returned with a body of channel context
- * 
+ *
  * CHANNEL_VOTE: a system message used to distribute votes within a channel
  *
  * Properties of:
@@ -56,7 +56,7 @@ export enum TYPE {
   DISCONNECT = 'disconnect',
   DELIVERY_CONFIRMATION = 'delivery-confirmation',
   INFO_CHANNEL_SYNC = 'info-channel-sync',
-  CHANNEL_VOTE = 'channel-vote'
+  CHANNEL_VOTE = 'channel-vote',
 }
 
 export enum TARGET {
