a03e0c96189aaec53d03069334bf91cd72969d39
NullVoxPopuli <LPSego3+dev@gmail.com>
2020-03-29 11:33:23 -0400

    Prep the test environment

--- packages/frontend/app/routes/setup.ts
+++ packages/frontend/app/routes/setup.ts
@@ -1,16 +1,7 @@
 import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
-
-import CurrentUserService from 'emberclear/services/current-user';
 
 export default class SetupIndexRoute extends Route {
-  @service currentUser!: CurrentUserService;
-
   async beforeModel() {
-    const exists = await this.currentUser.exists();
-
-    if (exists && !this.currentUser.allowOverride) {
-      this.transitionTo('setup.overwrite');
-    }
+    /* intentionally empty */
   }
 }
