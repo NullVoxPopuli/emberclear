78bbb9d7107bdf133c13c54e5688e5b630bf42b2
Austin Gassert <gasseram@rose-hulman.edu>
2020-03-30 11:38:01 -0400

    Finish boilerplate

--- packages/frontend/app/services/messages/command-handler.ts
+++ packages/frontend/app/services/messages/command-handler.ts
@@ -5,62 +5,124 @@ export default class CommandHandler extends Service {
     handleCommand(text: String) {
         const args = text.slice(1).trim().split(/ +/g);
         const command = args[0];
+
         // TODO channel
         // const channel = message.channel.name;
+
         // TODO author
         // const author = message.author;
 
         if (command === "state" || command === "s") {
-        // TODO 
+            this.printState();
         } 
 
         else if (command === "add-member" || command === "a") {
-        // TODO
+            this.addMember();
         }
 
         else if (command === "remove-member" || command === "r") {
-        // TODO
+            this.removeMember();
         }
 
         else if (command === "change-admin" || command === "p") {
-        // TODO
+            this.changeAdmin();
         }
 
         else if (command === "vote" || command === "v") {
-        // TODO
+            this.vote();
         }
 
         else if (command === "change-user-context-admin"  || command === "ucp") {
-        // TODO
+            this.changeUserContextAdmin();
         }
 
         else if (command === "change-user-context-add-member" || command === "uca") {
-        // TODO
+            this.changeUserContextAddMember();
         }
 
         else if (command === "change-user-context-remove-member" || command === "ucr") {
-        // TODO
+            this.changeUserContextRemoveMember();
         }
 
         else if (command === "view-user-context" || command === "ucv") {
-        // TODO
+            this.viewUserContext();
         }
         
         else if (command === "cancel-vote" || command === "cv") {
-        // TODO
+            this.cancelVote();
         }
 
         else if (command === "reset" || command === "rs") {
-        // TODO
+            this.reset();
         }
 
-        else if (command === "sync-discord-roles" || command === "d") {
-        // TODO
-        }
-        
         else {
-        // TODO help message
+            this.showHelp();
         }
     }
+
+    printState() {
+        // TODO
+    }
+
+    addMember() {
+        // TODO
+    }
     
+    removeMember() {
+        // TODO
+    }
+
+    changeAdmin() {
+        // TODO
+    }
+
+    vote() {
+        // TODO
+    }
+
+    changeUserContextAdmin() {
+        // TODO
+    }
+
+    changeUserContextAddMember() {
+        // TODO
+    }
+
+    changeUserContextRemoveMember() {
+        // TODO
+    }
+
+    viewUserContext() {
+        // TODO
+    }
+    
+    cancelVote() {
+        // TODO
+    }
+
+    reset() {
+        // TODO
+    }
+
+    showHelp() {
+        // TODO replace with system message
+        const commands = [
+                    "(t) test",
+					"(s) state",
+					"(a) add-member [user] [role]",
+					"(r) remove-member [user] [role]",
+					"(p) change-admin [user] [role]",
+					"(v) vote [yes/no] <user>",
+					"(c) create-channel [role]",
+					"(cv) cancel-vote",
+					"(ucp) change-user-context-admin [user] [role]",
+					"(uca) change-user-context-add-user [user] [role]",
+					"(ucr) change-user-context-remove-user [user] [role]",
+					"(ucv) view-user-context [user] [role]",
+					"(rs) reset",
+					"(d) sync-discord-roles [user]"
+        ];
+        alert(commands.join('\n'));
+    }
 }
