5f78b41b308c56021abc59c27baa4c4a0e92e394
NullVoxPopuli <LPSego3+dev@gmail.com>
2020-03-30 09:08:51 -0400

    whoops

---
+++ packages/frontend/app/routes/setup.ts
@@ -0,0 +1,7 @@
+import Route from '@ember/routing/route';
+
+export default class SetupRoute extends Route {
+  async beforeModel() {
+    /* intentionally empty due to some weird bug that I don't know about */
+  }
+}
