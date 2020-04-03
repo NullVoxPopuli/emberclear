9dafab172a7a2e65e831bd285fc225a24132fb1d
Hussein Alawami <alawamhm@rose-hulman.edu>
2020-04-03 16:51:47 -0400

    add tests for sign/open

--- packages/frontend/tests/workers/crypto/nacl-test.ts
+++ packages/frontend/tests/workers/crypto/nacl-test.ts
@@ -53,6 +53,31 @@ module('Workers | Crypto | nacl', function () {
     assert.deepEqual(msgAsUint8, decrypted);
   });
 
+  test('sign/open | works with Uint8Array', async function (assert) {
+    const sender = await nacl.generateAsymmetricKeys();
+
+    const msgAsUint8 = Uint8Array.from([104, 101, 108, 108, 111]); // hello
+    const signedText = await nacl.signMessage(msgAsUint8, sender.privateKey);
+    const openedText = await nacl.openSignedMessage(signedText, sender.publicKey);
+
+    assert.deepEqual(msgAsUint8, openedText);
+  });
+
+  test('sign/open | works with large data', async function (assert) {
+    const sender = await nacl.generateAsymmetricKeys();
+
+    let bigMsg: number[] = [];
+    for (let i = 0; i < 128; i++) {
+      bigMsg = bigMsg.concat([104, 101, 108, 108, 111]);
+    }
+
+    const msgAsUint8 = Uint8Array.from(bigMsg); // hello * 128 = 640 Bytes
+    const signedText = await nacl.signMessage(msgAsUint8, sender.privateKey);
+    const openedText = await nacl.openSignedMessage(signedText, sender.publicKey);
+
+    assert.deepEqual(msgAsUint8, openedText);
+  });
+
   test('splitNonceFromMessage | separates the nonce', async function (assert) {
     // prettier-ignore
     const msg = [
