f72040adab809cecd3bfede4dceba096fd6d0d65
CelticMajora <cdonoho16@gmail.com>
2020-04-08 17:55:14 -0400

    Extracted vote sorting into its own service since it will be used for both sending and receiving votes

---
+++ packages/frontend/tests/unit/service/channels/vote-sorter.ts
@@ -0,0 +1,29 @@
+import Service from '@ember/service';
+import VoteChain from 'emberclear/models/vote-chain';
+import Identity from 'emberclear/models/identity';
+
+export default class VoteSorter extends Service {
+  generateSortedVote(vote: VoteChain): Uint8Array {
+    let toReturn = [
+      this.toSortedPublicKeys(vote.remaining),
+      this.toSortedPublicKeys(vote.yes),
+      this.toSortedPublicKeys(vote.no),
+      vote.target.publicKey,
+      vote.action,
+      vote.key.publicKey,
+      vote.previousVoteChain.signature,
+    ];
+    return new TextEncoder().encode(JSON.stringify(toReturn));
+  }
+
+  private toSortedPublicKeys(identities: Identity[]): Uint8Array[] {
+    return identities.map((identity) => identity.publicKey).sort();
+  }
+}
+
+// DO NOT DELETE: this is how TypeScript knows how to look up your services.
+declare module '@ember/service' {
+  interface Registry {
+    'channels/vote-sorter': VoteSorter;
+  }
+}
--- packages/frontend/tests/unit/service/channels/vote-verifier-test.ts
+++ packages/frontend/tests/unit/service/channels/vote-verifier-test.ts
@@ -1,41 +1,40 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
-import td from 'testdouble';
 
 import { clearLocalStorage, getService, getStore } from 'emberclear/tests/helpers';
 import VoteVerifier from 'emberclear/services/channels/vote-verifier';
 
 module('Unit | Service | channels/vote-verifier', function (hooks) {
-    setupTest(hooks);
-    clearLocalStorage(hooks);
+  setupTest(hooks);
+  clearLocalStorage(hooks);
 
-    let service!: VoteVerifier;
+  let service!: VoteVerifier;
 
-    hooks.beforeEach(function () {
-        service = getService('channels/vote-verifier');
-    })
+  hooks.beforeEach(function () {
+    service = getService('channels/vote-verifier');
+  });
 
-    test('it exists', function(assert) {
-        assert.ok(service);
-    })
+  test('it exists', function (assert) {
+    assert.ok(service);
+  });
 
-    module('vote is valid', function (hooks) {
-        test('when only one chain', async function(assert) {
-            const store = getStore();
-            let currentVote = store.createRecord('vote-chain');
-            assert.ok(await service.verify(currentVote));
-        });
-
-        test('when there are many chains', async function(assert) {
-            const store = getStore();
-            let currentVote = store.createRecord('vote-chain');
-            let secondVote = store.createRecord('vote-chain');
-            let firstVote = store.createRecord('vote-chain');
-            currentVote.previousVoteChain = secondVote;
-            secondVote.previousVoteChain = firstVote;
-            firstVote.yes = [];
-            firstVote.no = [];
-            firstVote.remaining = [];
-        });
+  module('vote is valid', function (hooks) {
+    test('when only one chain', async function (assert) {
+      const store = getStore();
+      let currentVote = store.createRecord('vote-chain');
+      assert.ok(await service.verify(currentVote));
     });
+
+    test('when there are many chains', async function (assert) {
+      const store = getStore();
+      let currentVote = store.createRecord('vote-chain');
+      let secondVote = store.createRecord('vote-chain');
+      let firstVote = store.createRecord('vote-chain');
+      currentVote.previousVoteChain = secondVote;
+      secondVote.previousVoteChain = firstVote;
+      firstVote.yes = [];
+      firstVote.no = [];
+      firstVote.remaining = [];
+    });
+  });
 });
