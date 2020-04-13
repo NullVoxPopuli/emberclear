b03a13a5334bb9688cecc0227f3f73e2aacbb759
Taylor Chatfield <chatfitt@rose-hulman.edu>
2020-04-13 13:28:37 -0400

    adding responsepanel (broken), actions sidebar update

--- packages/frontend/app/components/app/sidebar/actions/responsepanel/index.ts
+++ packages/frontend/app/components/app/sidebar/actions/responsepanel/index.ts
@@ -4,19 +4,31 @@ import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
 
-import { selectUnreadDirectMessages } from 'emberclear/models/message/utils';
 import SettingsService from 'emberclear/services/settings';
 import SidebarService from 'emberclear/services/sidebar';
 import { TABLET_WIDTH } from 'emberclear/utils/breakpoints';
 import RouterService from '@ember/routing/router-service';
-import Action from 'emberclear/models/action';
+import Channel from 'emberclear/models/channel';
 import { currentUserId } from 'emberclear/services/current-user';
 
 interface IArgs {
-  action: Action;
+  channel: Channel;
 }
 
+export default class ResponsePanel extends Component<IArgs> {
 
-export default class SidebarAction extends Component<IArgs> {
+  @action
+  yes() {
+    alert("voting yes!");
+  }
 
+  @action
+  no() {
+    alert("voting no!");
+  }
+
+  @action
+  dismiss() {
+    alert("goodbye!");
+  }
 }
