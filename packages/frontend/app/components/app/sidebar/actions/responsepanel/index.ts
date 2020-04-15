d6098257e056d1351af56772ce9fde14a6b60446
Taylor Chatfield <chatfitt@rose-hulman.edu>
2020-04-15 14:42:19 -0400

    fixed handlebars function references, using buttons insead of anchors

--- packages/frontend/app/components/app/sidebar/actions/responsepanel/index.ts
+++ packages/frontend/app/components/app/sidebar/actions/responsepanel/index.ts
@@ -1,15 +1,8 @@
-import StoreService from '@ember-data/store';
 import Component from '@glimmer/component';
 
-import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
 
-import SettingsService from 'emberclear/services/settings';
-import SidebarService from 'emberclear/services/sidebar';
-import { TABLET_WIDTH } from 'emberclear/utils/breakpoints';
-import RouterService from '@ember/routing/router-service';
 import Channel from 'emberclear/models/channel';
-import { currentUserId } from 'emberclear/services/current-user';
 
 interface IArgs {
   channel: Channel;
