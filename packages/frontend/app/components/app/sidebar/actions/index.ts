aa1ba57130f5c213781188c0cdb12498e2076f1e
Taylor Chatfield <tchatfield16@gmail.com>
2020-04-20 13:15:58 -0400

    adding individual action elements

--- packages/frontend/app/components/app/sidebar/actions/index.ts
+++ packages/frontend/app/components/app/sidebar/actions/index.ts
@@ -1,5 +1,6 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 
 import StoreService from '@ember-data/store';
 import SettingsService from 'emberclear/services/settings';
@@ -7,6 +8,14 @@ import RouterService from '@ember/routing/router-service';
 import Action, { ACTION_RESPONSE } from 'emberclear/models/action';
 import CurrentUserService from 'emberclear/services/current-user';
 import SidebarService from 'emberclear/services/sidebar';
+
+import { VOTE_ACTION } from 'emberclear/models/vote-chain';
+import { generateSortedVote } from 'emberclear/services/channels/-utils/vote-sorter';
+import { sign, hash } from 'emberclear/workers/crypto/utils/nacl';
+
+import { toHex } from 'emberclear/utils/string-encoding';
+import { generateAsymmetricKeys, generateSigningKeys } from 'emberclear/workers/crypto/utils/nacl';
+import User from 'emberclear/models/user';
 
 interface IArgs {
   actions: Action[];
@@ -22,14 +31,58 @@ export default class ActionsSidebar extends Component<IArgs> {
 
   get allActions(): Action[] {
     //TODO: filter to only current channel
-    return this.store
+    /*return this.store
       .peekAll('action')
       .toArray()
-      .filter((action) => action.response !== ACTION_RESPONSE.DISMISSED);
+      .filter((action) => action.response !== ACTION_RESPONSE.DISMISSED);*/
   }
 
-  get actions() {
+  get actions(): Action[] {
+    /*
     return this.allActions.sort(sortByOldest);
+    */
+  }
+
+  @action
+  async addAction() {
+    //TODO: leave this to another method
+    /*
+    const name = 'user1';
+    const { publicKey } = await generateAsymmetricKeys();
+    const { publicSigningKey, privateSigningKey } = await generateSigningKeys();
+    const id = toHex(publicKey);
+
+    const defaultAttributes = { id, publicKey, publicSigningKey, privateSigningKey };
+
+    const record = this.store.createRecord('user', {
+      name,
+      ...defaultAttributes,
+    });
+
+    let user1 = record;
+    let userToAdd = record;
+
+    let currentVote = this.store.createRecord('vote-chain', {
+      yes: [],
+      no: [],
+      remaining: [user1],
+      action: VOTE_ACTION.ADD,
+      target: userToAdd,
+      key: user1,
+      previousVoteChain: undefined,
+      signature: undefined,
+    });
+
+    currentVote.signature = await sign(
+      await hash(generateSortedVote(currentVote)),
+      user1.privateSigningKey
+    );
+
+    this.store.createRecord('action', {
+      vote: currentVote,
+      response: ACTION_RESPONSE.NONE,
+      timestamp: Date.now(),
+    })*/
   }
 }
 
