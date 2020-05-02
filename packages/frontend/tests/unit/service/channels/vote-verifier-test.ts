50f3c26a18be6644debc762d3e9b750ca00461d2
CelticMajora <cdonoho16@gmail.com>
2020-05-02 17:07:37 -0400

    Added tests for invalid single vote chains

--- packages/frontend/tests/unit/service/channels/vote-verifier-test.ts
+++ packages/frontend/tests/unit/service/channels/vote-verifier-test.ts
@@ -105,63 +105,113 @@ module('Unit | Service | channels/vote-verifier', function (hooks) {
   });
 
   module('vote is not valid', function () {
-    test('when there are many chains', async function (assert) {
-      const store = getStore();
+    module('when there is a single chain', function () {
+      test('when first voter is in both yes and no', async function (assert) {
+        const store = getStore();
+        let user1 = await buildUser('user1');
+        let userToAdd = await buildUser('userToAdd');
 
-      let user1 = await buildUser('user1');
-      let user2 = await buildUser('user2');
-      let user3 = await buildUser('user3');
-      let user4 = await buildUser('user4');
-      let userToAdd = await buildUser('userToAdd');
+        let currentVote = store.createRecord('vote-chain', {
+          yes: [user1],
+          no: [user1],
+          remaining: [],
+          action: VOTE_ACTION.ADD,
+          target: userToAdd,
+          key: user1,
+          previousVoteChain: undefined,
+          signature: undefined,
+        });
+        currentVote.signature = await sign(
+          await hash(generateSortedVote(currentVote)),
+          user1.privateSigningKey
+        );
 
-      let firstVote = store.createRecord('vote-chain', {
-        yes: [user1],
-        no: [],
-        remaining: [user2, user3, user4],
-        action: VOTE_ACTION.ADD,
-        target: userToAdd,
-        key: user1,
-        previousVoteChain: undefined,
-        signature: undefined,
+        assert.notOk(await service.isValid(currentVote));
       });
-      firstVote.signature = await sign(
-        await hash(generateSortedVote(firstVote)),
-        user1.privateSigningKey
-      );
 
-      let secondVote = store.createRecord('vote-chain', {
-        yes: [user1, user2],
-        no: [],
-        remaining: [user3, user4],
-        action: VOTE_ACTION.ADD,
-        target: userToAdd,
-        key: user2,
-        previousVoteChain: firstVote,
-        signature: undefined,
+      test('when first voter is still in remaining', async function (assert) {
+        const store = getStore();
+        let user1 = await buildUser('user1');
+        let userToAdd = await buildUser('userToAdd');
+
+        let currentVote = store.createRecord('vote-chain', {
+          yes: [],
+          no: [],
+          remaining: [user1],
+          action: VOTE_ACTION.ADD,
+          target: userToAdd,
+          key: user1,
+          previousVoteChain: undefined,
+          signature: undefined,
+        });
+        currentVote.signature = await sign(
+          await hash(generateSortedVote(currentVote)),
+          user1.privateSigningKey
+        );
+
+        assert.notOk(await service.isValid(currentVote));
       });
-      secondVote.signature = await sign(
-        await hash(generateSortedVote(secondVote)),
-        user2.privateSigningKey
-      );
-      secondVote.no = [user4];
-      secondVote.remaining = [user3];
+    });
 
-      let currentVote = store.createRecord('vote-chain', {
-        yes: [user1, user2],
-        no: [user3, user4],
-        remaining: [],
-        action: VOTE_ACTION.ADD,
-        target: userToAdd,
-        key: user3,
-        previousVoteChain: secondVote,
-        signature: undefined,
+    module('when there are many chains', function () {
+      test('when a previous vote is modified', async function (assert) {
+        const store = getStore();
+
+        let user1 = await buildUser('user1');
+        let user2 = await buildUser('user2');
+        let user3 = await buildUser('user3');
+        let user4 = await buildUser('user4');
+        let userToAdd = await buildUser('userToAdd');
+
+        let firstVote = store.createRecord('vote-chain', {
+          yes: [user1],
+          no: [],
+          remaining: [user2, user3, user4],
+          action: VOTE_ACTION.ADD,
+          target: userToAdd,
+          key: user1,
+          previousVoteChain: undefined,
+          signature: undefined,
+        });
+        firstVote.signature = await sign(
+          await hash(generateSortedVote(firstVote)),
+          user1.privateSigningKey
+        );
+
+        let secondVote = store.createRecord('vote-chain', {
+          yes: [user1, user2],
+          no: [],
+          remaining: [user3, user4],
+          action: VOTE_ACTION.ADD,
+          target: userToAdd,
+          key: user2,
+          previousVoteChain: firstVote,
+          signature: undefined,
+        });
+        secondVote.signature = await sign(
+          await hash(generateSortedVote(secondVote)),
+          user2.privateSigningKey
+        );
+        secondVote.no = [user4];
+        secondVote.remaining = [user3];
+
+        let currentVote = store.createRecord('vote-chain', {
+          yes: [user1, user2],
+          no: [user3, user4],
+          remaining: [],
+          action: VOTE_ACTION.ADD,
+          target: userToAdd,
+          key: user3,
+          previousVoteChain: secondVote,
+          signature: undefined,
+        });
+        currentVote.signature = await sign(
+          await hash(generateSortedVote(currentVote)),
+          user3.privateSigningKey
+        );
+
+        assert.notOk(await service.isValid(currentVote));
       });
-      currentVote.signature = await sign(
-        await hash(generateSortedVote(currentVote)),
-        user3.privateSigningKey
-      );
-
-      assert.notOk(await service.isValid(currentVote));
     });
   });
 });
