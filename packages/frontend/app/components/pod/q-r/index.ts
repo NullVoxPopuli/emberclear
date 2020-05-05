a86470699d63a736db7d11ae60940c7020370336
NullVoxPopuli <LPSego3+dev@gmail.com>
2020-05-04 20:04:58 -0400

    successful use of useMachine

--- packages/frontend/app/components/pod/q-r/index.ts
+++ packages/frontend/app/components/pod/q-r/index.ts
@@ -1,12 +1,12 @@
 import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
-import { DEBUG } from '@glimmer/env';
 
-import { interpret, createMachine, Interpreter, State } from 'xstate';
+import { useMachine } from 'ember-statecharts';
+import { use } from 'ember-usable';
+
 import { machineConfig } from './-machine';
-import { Context, Schema, Events, LoginQRData } from './-types';
+import { LoginQRData } from './-types';
 
 import { SendDataConnection } from 'emberclear/services/connection/ephemeral/login/send-data';
 import CurrentUserService from 'emberclear/services/current-user';
@@ -20,54 +20,34 @@ export default class QRScan extends Component<Args> {
   @service currentUser!: CurrentUserService;
   @service qrManager!: QRManager;
 
-  @tracked current?: State<Context, Events, Schema>;
-
-  interpreter: Interpreter<Context, Schema, Events>;
+  @use interpreter = useMachine(machineConfig).withConfig({
+    services: {
+      setupConnection: this.setupConnection.bind(this),
+      transferData: this.transferData.bind(this),
+      addContact: this.addContact.bind(this),
+    },
+    guards: {
+      isQRLogin: ({ intent }) => intent === 'login',
+      isQRAddFriend: ({ intent }) => intent === 'add-friend',
+      hasError: ({ error }) => error === 'error',
+      isContactKnown: () => false,
+      isLoggedIn: () => this.currentUser.isLoggedIn,
+    },
+  });
 
   connection?: SendDataConnection;
 
-  constructor(owner: unknown, args: Args) {
-    super(owner, args);
-
-    let configuredMachine = createMachine<Context, Events>(machineConfig).withConfig({
-      services: {
-        setupConnection: this.setupConnection.bind(this),
-        transferData: this.transferData.bind(this),
-        addContact: this.addContact.bind(this),
-      },
-      guards: {
-        isQRLogin: ({ intent }) => intent === 'login',
-        isQRAddFriend: ({ intent }) => intent === 'add-friend',
-        hasError: ({ error }) => error === 'error',
-        isContactKnown: () => false,
-        isLoggedIn: () => this.currentUser.isLoggedIn,
-      },
-    });
-
-    this.interpreter = interpret(configuredMachine, { devTools: DEBUG });
-    this.interpreter
-      .onTransition((state) => (this.current = state))
-      .onDone(() => console.info('QR Wizard Completed'))
-      .start();
-  }
-
   get state() {
-    return this.current?.toStrings();
+    return this.interpreter?.currentState?.toStrings();
   }
 
   get ctx() {
-    return this.current?.context;
+    return this.interpreter?.currentState?.context;
   }
 
   @action
   handleScan(data: string) {
     this.interpreter.send('SCAN', { data });
-  }
-
-  willDestroy() {
-    super.willDestroy();
-
-    this.interpreter.stop();
   }
 
   @action
@@ -76,14 +56,14 @@ export default class QRScan extends Component<Args> {
   }
 
   async setupConnection() {
-    if (!this.current) {
+    if (!this.interpreter?.currentState) {
       throw new Error('No State' /* but what we make */);
     }
 
     // The State-Machine prevents this method from being called
     // without an existing `pub` (which is always present when)
     // the `intent` is "login"
-    let { pub } = this.current.context.data as LoginQRData[1];
+    let { pub } = this.interpreter?.currentState?.context.data as LoginQRData[1];
 
     this.connection = await this.qrManager.login.setupConnection(this, pub);
   }
