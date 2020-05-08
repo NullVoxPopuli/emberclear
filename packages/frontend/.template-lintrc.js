22193696818b7fcb0612f46821208c1ae26f183e
NullVoxPopuli <LPSego3+dev@gmail.com>
2020-05-07 22:29:36 -0400

    cleanup the template lint config

--- packages/frontend/.template-lintrc.js
+++ packages/frontend/.template-lintrc.js
@@ -5,40 +5,5 @@ module.exports = {
   rules: {
     quotes: 'single',
     'no-inline-styles': false, // TODO enable :(
-    'no-implicit-this': true,
-    'no-bare-strings': {
-      // ugh
-      whitelist: [
-        '&nbsp;',
-        '(',
-        ')',
-        ',',
-        '.',
-        '&',
-        '+',
-        '-',
-        '=',
-        '*',
-        '/',
-        '#',
-        '%',
-        '!',
-        '?',
-        ':',
-        '[',
-        ']',
-        '{',
-        '}',
-        '<',
-        '>',
-        '•',
-        '—',
-        ' ',
-        '|',
-        '@',
-      ],
-    },
-
-    'attribute-indentation': false,
   },
 };
