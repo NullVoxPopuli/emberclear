196055f4f110d826abe3020696b3ea4665c4dbcc
CelticMajora <cdonoho16@gmail.com>
2020-05-16 19:17:21 -0400

    Added test for making sure signing keys are generated

--- packages/frontend/tests/unit/service/current-user-test.ts
+++ packages/frontend/tests/unit/service/current-user-test.ts
@@ -1,8 +1,25 @@
 import { module, skip } from 'qunit';
-import { setupTest } from 'ember-qunit';
+import { setupTest, test } from 'ember-qunit';
+import CurrentUserService from 'emberclear/services/current-user';
+import { getService } from 'emberclear/tests/helpers';
+import { generateAsymmetricKeys } from 'emberclear/workers/crypto/utils/nacl';
 
 module('Unit | Service | identity', function (hooks) {
   setupTest(hooks);
+
+  let service: CurrentUserService;
+
+  hooks.beforeEach(() => {
+    service = getService('current-user');
+  });
+
+  test('importFromKey where privateSigningKey is not present generates signing keys', async function (assert) {
+    let keys = await generateAsymmetricKeys();
+    await service.importFromKey('name', keys.privateKey);
+    assert.notStrictEqual(await service.currentUser(), null);
+    assert.ok((await service.currentUser())?.publicSigningKey);
+    assert.ok((await service.currentUser())?.privateSigningKey);
+  });
 
   skip('can dump and reload', async function (assert) {
     assert.expect(0);
