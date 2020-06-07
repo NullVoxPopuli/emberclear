27f9bbe90fedb0242f50dbc353a1614a1de4cce2
NullVoxPopuli <LPSego3+dev@gmail.com>
2020-06-07 15:46:41 -0400

    fix taskFor helper method for new async tasks

--- client/web/emberclear/app/utils/ember-concurrency.ts
+++ client/web/emberclear/app/utils/ember-concurrency.ts
@@ -2,9 +2,7 @@ import Task from 'ember-concurrency/task';
 import { didCancel } from 'ember-concurrency';
 import { Event } from 'xstate';
 
-type ECTask<Args extends Array<any>, Return> = (
-  ...args: Args
-) => Generator<any /* potentially yielded types */, Return, unknown>;
+type ECTask<Args extends Array<any>, Return> = (...args: Args) => Promise<Return>;
 
 export function taskFor<Args extends any[], Return = void>(generatorFn: ECTask<Args, Return>) {
   return (generatorFn as any) as Task<Args, Return>;
