d19aeb62ccc55e3faed096a5d6666cf1ef2d0d18
NullVoxPopuli <LPSego3+dev@gmail.com>
2020-06-27 18:40:17 -0400

    improve media rendering

---
+++ client/web/addons/ui/tests/integration/components/media-info-card-test.ts
@@ -0,0 +1,8 @@
+// import { module, test } from 'qunit';
+// import { setupRenderingTest } from 'ember-qunit';
+// import { render } from '@ember/test-helpers';
+// import hbs from 'htmlbars-inline-precompile';
+
+// module('Integration | Component | media-info-card', function (hooks) {
+//   setupRenderingTest(hooks);
+// });
