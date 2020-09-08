7f20cb8e2bb3f1e3e09edd1c58791c9d59c9942d
NullVoxPopuli <LPSego3+dev@gmail.com>
2020-09-07 20:34:28 -0400

    fix mobile menu?

--- client/web/emberclear/app/components/app/off-canvas/index.ts
+++ client/web/emberclear/app/components/app/off-canvas/index.ts
@@ -1,11 +1,36 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 
-import type CurrentUserService from 'emberclear/services/current-user';
+import { TABLET_WIDTH } from 'emberclear/utils/breakpoints';
 
+import type CurrentUserService from 'emberclear/services/current-user';
 import type Sidebar from 'emberclear/services/sidebar';
 
 export default class OffCanvasContainer extends Component {
-  @service currentUser!: CurrentUserService;
-  @service sidebar!: Sidebar;
+  @service declare currentUser: CurrentUserService;
+  @service declare sidebar: Sidebar;
+  // @service declare window: Window;
+
+  @tracked lastWindowWidth = window.innerWidth;
+
+  get mode() {
+    if (this.isTabletOrSmaller) {
+      return 'reveal';
+    }
+
+    return 'squeeze-reveal';
+  }
+
+  // window.screen => physical screen
+  // window.innerWidth => full width of window, excluding dev tools
+  get isTabletOrSmaller() {
+    return this.lastWindowWidth <= TABLET_WIDTH;
+  }
+
+  @action
+  updateWindowWidth() {
+    this.lastWindowWidth = window.innerWidth;
+  }
 }
