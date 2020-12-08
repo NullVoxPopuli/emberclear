459e710e6427091e9127e15d61c3c07ee334f771
NullVoxPopuli <LPSego3+dev@gmail.com>
2020-12-07 21:25:07 -0500

    fix resize observer

--- client/web/emberclear/app/components/app/off-canvas/index.ts
+++ client/web/emberclear/app/components/app/off-canvas/index.ts
@@ -2,18 +2,18 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
+import { debounce } from '@ember/runloop';
 
 import { TABLET_WIDTH } from 'emberclear/utils/breakpoints';
 
 import type CurrentUserService from 'emberclear/services/current-user';
 import type Sidebar from 'emberclear/services/sidebar';
 
-export default class OffCanvasContainer extends Component {
+type Args = Record<string, unknown>;
+
+export default class OffCanvasContainer extends Component<Args> {
   @service declare currentUser: CurrentUserService;
   @service declare sidebar: Sidebar;
-  // @service declare window: Window;
-
-  @tracked lastWindowWidth = window.innerWidth;
 
   get mode() {
     if (this.isTabletOrSmaller) {
@@ -23,6 +23,15 @@ export default class OffCanvasContainer extends Component {
     return 'squeeze-reveal';
   }
 
+  // ----------------------------------------
+  // TODO: extract all this to a resource?
+  // ----------------------------------------
+  declare handler: any; // where is ember run timer?
+
+  // @service declare window: Window;
+
+  @tracked lastWindowWidth = window.innerWidth;
+
   // window.screen => physical screen
   // window.innerWidth => full width of window, excluding dev tools
   get isTabletOrSmaller() {
@@ -30,7 +39,19 @@ export default class OffCanvasContainer extends Component {
   }
 
   @action
-  updateWindowWidth() {
+  updateWidth() {
     this.lastWindowWidth = window.innerWidth;
+  }
+
+  constructor(owner: unknown, args: Args) {
+    super(owner, args);
+
+    this.handler = () => debounce(this, 'updateWidth', 100);
+
+    window.addEventListener('resize', this.handler);
+  }
+
+  willDestroy() {
+    window.removeEventListener('resize', this.handler);
   }
 }
