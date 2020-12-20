5f8277d3962d9361c7e27e6cce6398fce3157285
NullVoxPopuli <LPSego3+dev@gmail.com>
2020-12-20 13:03:10 -0500

    Fix types for invokeHelper

--- client/web/pinochle/app/components/options.ts
+++ client/web/pinochle/app/components/options.ts
@@ -1,4 +1,5 @@
 import Component from '@glimmer/component';
+import Helper, { invokeHelper } from '@ember/component/helper';
 import { action } from '@ember/object';
 
 import { inLocalStorage } from 'ember-tracked-local-storage';
@@ -9,5 +10,24 @@ export default class Options extends Component {
   @action
   toggle() {
     this.isSynthwave = !this.isSynthwave;
+  }
+
+  applyBodyClass = invokeHelper(this, ApplyBodyClass, () => [this.isSynthwave]);
+}
+
+type PositionalArgs = [string];
+
+class ApplyBodyClass extends Helper {
+  declare className: string;
+  compute([className]: PositionalArgs) {
+    this.className = className;
+
+    document.body.classList.add(className);
+  }
+
+  willDestroy() {
+    if (this.className) {
+      document.body.classList.remove(this.className);
+    }
   }
 }
