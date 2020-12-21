0afc2350f6b1a2bffcb738fc509b85a559b8ac0a
NullVoxPopuli <LPSego3+dev@gmail.com>
2020-12-21 08:46:42 -0500

    Setup typecsript and new deps for new addon

---
+++ client/web/addons/test-helpers/addon-test-support/-private/get-service.ts
@@ -0,0 +1,11 @@
+import { getContext } from '@ember/test-helpers';
+
+import type { Registry } from '@ember/service';
+
+export function getService<K extends keyof Registry>(name: K): Registry[K] {
+  const { owner } = getContext() as any;
+
+  const service = owner.lookup(`service:${name}`);
+
+  return service as Registry[K];
+}
--- client/web/emberclear/tests/helpers/get-service.ts
+++ client/web/emberclear/tests/helpers/get-service.ts
@@ -3,14 +3,4 @@
 import 'emberclear/services/redirect-manager';
 import 'emberclear/services/chat-scroller';
 
-import { getContext } from '@ember/test-helpers';
-
-import type { Registry } from '@ember/service';
-
-export function getService<K extends keyof Registry>(name: K): Registry[K] {
-  const { owner } = getContext() as any;
-
-  const service = owner.lookup(`service:${name}`);
-
-  return service as Registry[K];
-}
+export { getService } from '@emberclear/test-helpers/test-support';
